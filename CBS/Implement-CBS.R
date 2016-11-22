9bcea0a695f9f858dca8dda779da42779310cbb0
swati kaushik <swati.kaushik15@gmail.com>
2016-11-22 12:34:54 -0800

    First commit

---
+++ CBS/Implement-CBS.R
@@ -0,0 +1,16 @@
+library(PSCBS)
+
+#load test dataset
+
+data <- read.table("sample_data", header=T)
+data <- data[, c("chromosome", "x", "CT")]
+
+# drop outliers 
+data <- dropSegmentationOutliers(data)
+
+#removing gaps
+gaps <- findLargeGaps(data, minLength = 1e+01)
+knownSegments <- gapsToSegments(gaps)
+
+#segmenting 
+fit <- segmentByCBS(data, knownSegments = knownSegments, seed = 48879, verbose = -10)
